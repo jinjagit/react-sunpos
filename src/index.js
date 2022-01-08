c394beed562d8275e5a4e1eff311b8b80b869f86
Simon Tharby <simontharby@gmail.com>
2022-01-08 13:30:01 +0000

    Add two inputs and display result in output component
    Could DRY this out, by reusing a common input component, but
    these will become very different components, so let's not.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // TODO: Add styling
 
-class Input extends React.Component {
+class InputA extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {inputValue: ''};
+    this.state = {inputValue: 0};
     this.handleChange.bind(this);
+    this.props.setStateOfPageA(0);
   }
 
   handleChange = (evt) => {
     this.setState({inputValue: evt.target.value});
-    this.props.setStateOfPage(evt.target.value);
+    this.props.setStateOfPageA(parseFloat(evt.target.value));
+  }
+
+  render() {
+    return (
+      <input type="number"
+        value={this.state.inputValue}
+        onChange={evt => this.handleChange(evt)}
+      >
+      </input>
+    );
+  }
+}
+
+class InputB extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {inputValue: 0};
+    this.handleChange.bind(this);
+    this.props.setStateOfPageB(0);
+  }
+
+  handleChange = (evt) => {
+    this.setState({inputValue: evt.target.value});
+    this.props.setStateOfPageB(parseFloat(evt.target.value));
   }
 
   render() {
@@ -36,25 +61,34 @@ class Output extends React.Component {
 class Page extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {inputVal: 0};
-    this.setStateOfPage.bind(this);
+    this.state = {inputValA: 0};
+    this.state = {inputValB: 0};
+    this.setStateOfPageA.bind(this);
+    this.setStateOfPageB.bind(this);
   }
 
-  setStateOfPage = (newValue) => {
-    this.setState({inputVal: newValue});
+  setStateOfPageA = (newValue) => {
+    this.setState({inputValA: newValue});
+  }
+
+  setStateOfPageB = (newValue) => {
+    this.setState({inputValB: newValue});
   }
 
   renderOutput() {
     return (
-      <Output value={this.state.inputVal}/>
+      <Output value={(this.state.inputValA + this.state.inputValB)}/>
     );
   }
 
   render() {
     return (
       <div className="page">
-        <div className="input">
-          <Input setStateOfPage = {this.setStateOfPage}/>
+        <div className="inputA">
+          <InputA setStateOfPageA = {this.setStateOfPageA}/>
+        </div>
+        <div className="inputA">
+          <InputB setStateOfPageB = {this.setStateOfPageB}/>
         </div>
         <div className="output">
           {this.renderOutput()}
