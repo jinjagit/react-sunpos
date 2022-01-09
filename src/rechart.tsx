2a5c190043164e6d7010db0a6325cf8b4dc04166
Simon Tharby <simontharby@gmail.com>
2022-01-09 23:33:34 +0000

    remove unusued import

--- src/rechart.tsx
+++ src/rechart.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 interface Props {
   a: number,
