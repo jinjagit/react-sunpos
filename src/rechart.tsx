a4675deb65c2c9a1389f02b95aa2a34d0f1f312b
Simon Tharby <simontharby@gmail.com>
2022-01-10 08:55:24 +0000

    increase line stroke width

--- src/rechart.tsx
+++ src/rechart.tsx
@@ -34,7 +34,7 @@ export default class Rechart extends PureComponent <Props> {
           <XAxis dataKey="x" type="number" domain={[0, 10]} tickCount={11}/>
           <YAxis dataKey="y" type="number" domain={[0, 10]} tickCount={11}/>
           <Tooltip />
-          <Line type="monotone" dataKey="y" stroke="#8884d8" activeDot={{ r: 6 }} isAnimationActive={false} />
+          <Line type="monotone" dataKey="y" stroke="#8884d8" activeDot={{ r: 6 }} isAnimationActive={false} strokeWidth={3} />
         </LineChart>
       </ResponsiveContainer>
     );
