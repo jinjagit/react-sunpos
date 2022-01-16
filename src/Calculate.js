8829ba0b0f2b9386e6328eccea3ca478515a5459
Simon Tharby <simontharby@gmail.com>
2022-01-16 17:35:50 +0000

    always show all ticks (bad for mobile)

--- src/Calculate.js
+++ src/Calculate.js
@@ -8,12 +8,12 @@ class Calculate extends Component {
       let data = sunPath(this.props.params.date, parseFloat(this.props.params.latitude), parseFloat(this.props.params.longitude), parseInt(this.props.params.utcOffset));
 
       return (
-        <div>
+        <div className='container chartBox'>
           <div className='container'>
             <p>Last parameters received: {JSON.stringify(this.props.params)}</p>
           </div>
           <div className='container chart'>
-            <h5>Time (minutes) x Inclination (degrees above/below horizon)</h5>
+            <h5>Time of day x Inclination (degrees above/below horizon)</h5>
             <Chart data={data} />
           </div>
         </div>
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine, ResponsiveContainer } from 'recharts';
 
 interface Props {
   data: {
@@ -27,10 +27,22 @@ export default class Chart extends PureComponent <Props> {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" type="category"/>
+          <XAxis
+            dataKey="time"
+            type="category"
+            ticks={[
+              '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
+              '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
+              '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
+              '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+              '23:59'
+            ]}
+            interval={0}
+          />
           <YAxis />
           <Tooltip />
-          <Line type="monotone" dataKey="sza" stroke="#8884d8" activeDot={false} strokeWidth={1} dot={false} />
+          <Line type="monotone" dataKey="sza" stroke="#8884d8" activeDot={false} strokeWidth={2} dot={false} />
+          <ReferenceLine y="0.0" stroke="#adadad" />
         </LineChart>
       </ResponsiveContainer>
     );
